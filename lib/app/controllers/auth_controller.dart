58dc5060f62942b3ab63ed52cdbc8ddb0551ef73
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-21 17:50:42 +0700

    Menyempurnakan AuthController untuk handle authentication Serta melakukkan pembungkusan materialapp kedalam stream builder agar kita bisa menggunakan authC sebagai stream saat inisialisasi applikasi

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -4,6 +4,12 @@ import 'package:get/get.dart';
 class AuthController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
 
+  // Stream<User?> streamAuthStatus() {
+  //   return auth.authStateChanges();
+  // } *** VERSI KOMPLEKS
+
+  Stream<User?> get streamAuthStatus => auth.authStateChanges(); // VERSI SIMPLE
+
   void signup() {}
   void login() {}
   void logout() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_firebase_app/app/controllers/auth_controller.dart';
+import 'package:my_firebase_app/app/utils/loading.dart';
 import 'app/routes/app_pages.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -10,11 +15,33 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final authC = Get.put(
+    AuthController(),
+    permanent: true,
+  ); // diakan selalu dijalankan dikondisi apapun
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: authC.streamAuthStatus,
+      builder: (context, snapshot) {
+        print(snapshot);
+        if (snapshot.connectionState == ConnectionState.active) {
+          print(snapshot.data); // jika null maka user belum login.
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }
+        return LoadingView();
+      },
+    );
+  }
 }
